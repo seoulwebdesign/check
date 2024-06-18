96e058b482674929234cca5fcdca1c8986b3548d
Stevie <mods2003@gmail.com>
2024-06-18 22:20:50 +0900

    add

---
+++ app/fail/page.tsx
@@ -0,0 +1,9 @@
+const Fail = () => (
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold mb-4">Payment Failed</h1>
+      <p>Unfortunately, your payment could not be processed. Please try again.</p>
+    </div>
+  );
+  
+  export default Fail;
+  
---
+++ app/success/page.tsx
@@ -0,0 +1,9 @@
+const Success = () => (
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold mb-4">Payment Successful</h1>
+      <p>Your payment was successful. Thank you for your purchase!</p>
+    </div>
+  );
+  
+  export default Success;
+  
